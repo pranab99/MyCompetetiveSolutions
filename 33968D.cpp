a3538befe4dfc0ea6dfaef6753e778ccd6adaedc
pranab1999 <impranabdas99@gmail.com>
2021-06-30 07:52:45 +0530

    Added more

--- 33968D.cpp
+++ 33968D.cpp
@@ -9,16 +9,55 @@ float roundoff(float value, unsigned char prec) {
 }
 
 
-
+bool isPrime(int n)
+{
+    // Corner cases
+    if (n <= 1)
+        return false;
+    if (n <= 3)
+        return true;
+  
+    // This is checked so that we can skip
+    // middle five numbers in below loop
+    if (n % 2 == 0 || n % 3 == 0)
+        return false;
+  
+    for (int i = 5; i * i <= n; i = i + 6)
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+  
+    return true;
+}
 
 void solve() {
-    
 
-   int n;
-   cin>>n;
-   cout<<n;
 
-}
+	int n, m = 0, ct = 0;
+	cin >> n;
+
+	for(int i = 1; i <= n; i++) {
+
+		for(int j = 2; j <= i ; j++) {
+			
+			if(isPrime(j)) {
+				if(i % j == 0) {
+					ct++;
+				}
+
+			}
+		}
+
+		if(ct == 2) {
+			m++;
+		}
+
+		ct = 0;
+
+	}
+    cout<<m;
+		
+	}
+
 
 
 
--- atcoder.cpp
+++ atcoder.cpp
@@ -3,33 +3,34 @@
 #define ll long long
 using namespace std;
 
-float roundoff(float value, unsigned char prec)
-{
-  float pow_10 = pow(10.0f, (float)prec);
-  return round(value * pow_10) / pow_10;
+float roundoff(float value, unsigned char prec) {
+   float pow_10 = pow(10.0f, (float)prec);
+   return round(value * pow_10) / pow_10;
 }
 
-void solve()
-{
+void solve() {
 
-  
-   
+
+
+}
+
+signed main() {
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);
+#ifdef Pranab
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+
+   int t = 1;
+   // cin>>t;
+
+   while(t--) {
+      solve();
+      cout << "\n";
    }
 
-signed main()
-{
-   ios_base::sync_with_stdio(false);cin.tie(NULL);
-   #ifdef Pranab
-   freopen("input.txt", "r", stdin);freopen("output.txt", "w", stdout);
-   #endif  
- 
-   int t=1; 
-  // cin>>t;
- 
-   while(t--)
-   {solve();cout<<"\n";}
-
-   cerr<<"\n"<<(float)clock()/CLOCKS_PER_SEC*1000<<" ms"<<endl;
+   cerr << "\n" << (float)clock() / CLOCKS_PER_SEC * 1000 << " ms" << endl;
 
    return 0;
 }
