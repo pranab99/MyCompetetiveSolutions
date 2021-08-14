b5ebca236edc953f70a898bddb88ce2b8f87f5b1
pranab1999 <impranabdas99@gmail.com>
2021-08-14 19:05:08 +0530

    AF

--- atcoder.cpp
+++ atcoder.cpp
@@ -3,48 +3,55 @@
 #define ll long long
 using namespace std;
 
-void printSubsequence(string input, string output)
-{
-
-    if (input.empty()) {
-        cout << output << endl;
-        return;
-    }
- 
-
-    printSubsequence(input.substr(1), output + input[0]);
- 
-
-    printSubsequence(input.substr(1), output);
+float roundoff(float value, unsigned char prec) {
+	float pow_10 = pow(10.0f, (float)prec);
+	return round(value * pow_10) / pow_10;
 }
+
+
+
 
 void solve() {
- string s,c="chokudai";
 
-cin>>s;
- printSubsequence(s,c);
+ll s,t,ct=0;
+cin>>s>>t;
 
-  
- 
+for (int i = 0; i <=s ; ++i)
+{
+  for( int j=0 ; j<=s ; ++j){
+  	for (int k = 0; k <=s; ++k)
+  	{
+  	 if(i+j+k<=s && i*j*k<=t)
+  	 	ct++;
+  	}
+  }
 }
 
+cout<<ct;
+
+}
+
+
+
+
+
 signed main() {
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 #ifdef Pranab
-   freopen("input.txt", "r", stdin);
-   freopen("output.txt", "w", stdout);
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
 #endif
 
-   int t = 1;
-   // cin>>t;
+	int t = 1;
+	// cin>>t;
 
-   while(t--) {
-      solve();
-      cout << "\n";
-   }
+	while(t--) {
+		solve();
+		cout << "\n";
+	}
 
-   cerr << "\n" << (float)clock() / CLOCKS_PER_SEC * 1000 << " ms" << endl;
+	cerr << "\n" << (float)clock() / CLOCKS_PER_SEC * 1000 << " ms" << endl;
 
-   return 0;
+	return 0;
 }
